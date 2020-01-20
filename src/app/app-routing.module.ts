4c9dfbe52879b4a10adabfd3d5637fff322deba7
Don Moses <githinjidon@gmail.com>
2020-01-20 10:54:34 +0300

    specify routes to take

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchformComponent } from './components/search-form/search-form.component';
 import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-  { path: 'search', component: SearchFormComponent},
+  { path: 'search', component: SearchformComponent},
   { path: 'about', component: AboutComponent},
 ];
 
